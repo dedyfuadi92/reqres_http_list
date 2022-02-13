f98039de77354276922469a995fc242689567910
dedyfuadi92 <dedyfuadi92@gmail.com>
2022-02-13 22:58:19 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/reqres_http_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reqres_http_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:reqres_http_list/user_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String output = 'Tidak ada data';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GET LIST'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                output,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  User.getUser('1').then((users) {
+                    output = '';
+                    for (var i = 0; i < users.length; i++) {
+                      output = output + '[' + users[i].name + ']\n\n';
+                    }
+                    setState(() {});
+                  });
+                },
+                child: Text('GET'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
